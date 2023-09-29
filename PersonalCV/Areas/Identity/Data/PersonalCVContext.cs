e85adfce268509ccca46bf6101de5fe81962cd29
AJBoy\AJ Boy <jebaleali@gmail.com>
2023-09-29 12:20:41 +0330

    add asp.net identity seed data add migration

--- PersonalCV/Areas/Identity/Data/PersonalCVContext.cs
+++ PersonalCV/Areas/Identity/Data/PersonalCVContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PersonalCV.Domain.Models;
 
 namespace PersonalCV.Data;
 
@@ -11,11 +12,26 @@ public class PersonalCVContext : IdentityDbContext<IdentityUser>
     {
     }
 
+    public DbSet<ContactMessage> ContactMessages { get; set; }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        var appUser = new IdentityUser
+        {
+            Id = Guid.NewGuid().ToString(),
+            Email = "jebaleali@gmail.com",
+            EmailConfirmed = true,
+            UserName = "jebaleali@gmail.com",
+            NormalizedUserName = "JEBALEALI@GMAIL.COM"
+        };
+
+        //set user password
+        PasswordHasher<IdentityUser> ph = new PasswordHasher<IdentityUser>();
+        appUser.PasswordHash = ph.HashPassword(appUser, "alijebale");
+
+        //seed user
+        builder.Entity<IdentityUser>().HasData(appUser);
+
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
     }
 }
--- PersonalCV/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PersonalCV/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,21 +73,26 @@ public class LoginModel : PageModel
         public bool RememberMe { get; set; }
     }
 
-    public async Task OnGetAsync(string returnUrl = null)
+    public async Task<IActionResult> OnGetAsync(string returnUrl = null)
     {
         if (!string.IsNullOrEmpty(ErrorMessage))
         {
             ModelState.AddModelError(string.Empty, ErrorMessage);
         }
 
-        returnUrl ??= Url.Content("~/");
+        returnUrl ??= Url.Content("~/Admin");
 
         // Clear the existing external cookie to ensure a clean login process
         await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-        ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
         ReturnUrl = returnUrl;
+
+        if (User.Identity.IsAuthenticated)
+        {
+            return LocalRedirect(returnUrl);
+        }
+
+        return Page();
     }
 
     public async Task<IActionResult> OnPostAsync(string returnUrl = null)
